f9ae97580a5ee85c2434e04bef1c6249f50b8e84
chris.bridges <christopher_bridges@icloud.com>
2018-06-25 22:53:16 -0400

    mobile MVP

---
+++ app.js
@@ -0,0 +1,8 @@
+const options = {
+  strings: ['programmer', 'leader', 'musician', 'marketer', 'mentor', 'analyst', 'problem solver'],
+  typeSpeed: 50,
+  showCursor: true,
+  backDelay: 1000
+}
+
+const typed = new Typed("#i-am-a-list", options);
