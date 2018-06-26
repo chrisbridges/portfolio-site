7e13fda704ce19eaee722eb28b386e72adf6f62b
chris.bridges <christopher_bridges@icloud.com>
2018-06-26 11:31:41 -0400

    cut the fluff. spaced things more appropriately for desktops

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ function typed () {
   strings: ['programmer', 'leader', 'musician', 'marketer', 'mentor', 'analyst', 'problem solver'],
   typeSpeed: 50,
   showCursor: true,
-  backDelay: 1000
+  backDelay: 1000,
+  smartBackspace: false
 }
   const typed = new Typed("#i-am-a-list", options);
 }
@@ -28,7 +29,7 @@ function fadeInSkills () {
     }); 
 
   });
-  
+
 }
 
 
