97f9f9a2c38fd74f0da0d67de674df106f089643
Chris Bridges <christopher_bridges@icloud.com>
2024-03-21 16:59:56 -0500

    merge addressed

--- app.js
+++ app.js
@@ -1,92 +1,92 @@
-
-function fadeInSkills () {
+function fadeInSkills() {
   /* Every time the window is scrolled ... */
-  $(window).scroll( function(){
+  $(window).scroll(function () {
     /* Check the location of each desired element */
-    $('.tech-skills-icon').each( function(i){
+    $(".tech-skills-icon").each(function (i) {
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       /* If the object is completely visible in the window, fade it in */
       if (bottom_of_window > bottom_of_object) {
-        $(this).animate({'opacity':'1'},4000);  
+        $(this).animate({ opacity: "1" }, 4000);
       }
-    }); 
+    });
   });
 }
 
-function getStartedClick () {
-  $('.get-started').click(function(){
+function getStartedClick() {
+  $(".get-started").click(function () {
     let aTag = $("#about-me");
     // headerPadding value chosen because it seemed to look best
     let headerPadding = 18;
-    $('html,body').animate({scrollTop: aTag.offset().top + headerPadding},'slow');
+    $("html,body").animate(
+      { scrollTop: aTag.offset().top + headerPadding },
+      "slow"
+    );
   });
 }
 
-function menuNavigation () {
+function menuNavigation() {
   const menuHash = {
-    "Home": "#intro",
+    Home: "#intro",
     "My Story": "#about-me",
-    "Skills": "#skills",
-    "Projects": "#projects",
+    Skills: "#skills",
+    Projects: "#projects",
     "Experience & Education": "#experience-and-education",
-<<<<<<< Updated upstream
-    "References": "#references",
-    "Get in Touch": "#contact-me"
-=======
     References: "#references",
     Contact: "#contact-me",
->>>>>>> Stashed changes
   };
 
-  $('.menu-options ul li').on('click', function (e) {
+  $(".menu-options ul li").on("click", function (e) {
     e.preventDefault();
     let navigateTo = $(e.currentTarget).text();
     let aTag = $(menuHash[navigateTo]);
     let headerPadding = 18;
     if (navigateTo === "Home") {
       // if navigating home, provide no padding-top
-      $('html,body').animate({scrollTop: 0},'slow');
+      $("html,body").animate({ scrollTop: 0 }, "slow");
     } else {
-      $('html,body').animate({scrollTop: aTag.offset().top + headerPadding},'slow');
+      $("html,body").animate(
+        { scrollTop: aTag.offset().top + headerPadding },
+        "slow"
+      );
     }
   });
 }
 
 // TODO: create func where if menu is open, and click is outside menu, close menu
 
-function openMenu () {
-  $('.hamburger-menu').click(function () {
-    if ($('.menu-options').is(':hidden')) {
-      $('.menu-options').slideDown();
+function openMenu() {
+  $(".hamburger-menu").click(function () {
+    if ($(".menu-options").is(":hidden")) {
+      $(".menu-options").slideDown();
     } else {
-      $('.menu-options').slideUp();
+      $(".menu-options").slideUp();
     }
   });
 }
 
 // font-color of 'My Story' section header is originally white
-  // change to black on scroll so it's visible against white sticky heading
-function changeMyStoryColor () {
-  $(window).on("scroll", function() {
-    var aboutMeHeaderPosition = $('#about-me').offset().top;
+// change to black on scroll so it's visible against white sticky heading
+function changeMyStoryColor() {
+  $(window).on("scroll", function () {
+    var aboutMeHeaderPosition = $("#about-me").offset().top;
     if ($(window).scrollTop() > aboutMeHeaderPosition) {
-        $("#about-me h2").addClass("active");
+      $("#about-me h2").addClass("active");
     } else {
-        //remove the background property so it comes transparent again (defined in your css)
-       $("#about-me h2").removeClass("active");
+      //remove the background property so it comes transparent again (defined in your css)
+      $("#about-me h2").removeClass("active");
     }
   });
 }
 
-function changeReferencesColor () {
-  $(window).on("scroll", function() {
-    let referencesHeaderPosition = $('#references').offset().top;
+function changeReferencesColor() {
+  $(window).on("scroll", function () {
+    let referencesHeaderPosition = $("#references").offset().top;
     if ($(window).scrollTop() > referencesHeaderPosition) {
-        $("#references h2").addClass("active");
+      $("#references h2").addClass("active");
     } else {
-        //remove the background property so it comes transparent again (defined in your css)
-       $("#references h2").removeClass("active");
+      //remove the background property so it comes transparent again (defined in your css)
+      $("#references h2").removeClass("active");
     }
   });
 }
